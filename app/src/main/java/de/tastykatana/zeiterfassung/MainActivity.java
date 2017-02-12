46ecc68a271e41f22455d3225ed6873017ad6d8d
Marrrrtthias <matthiasgellrich@gmail.com>
2017-02-12 18:03:16 +0100

    basics for stundenzettel-export added

--- app/src/main/java/de/tastykatana/zeiterfassung/MainActivity.java
+++ app/src/main/java/de/tastykatana/zeiterfassung/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +16,7 @@ import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.LibsBuilder;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -59,17 +62,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * exports the Stundenzettel to /data/data/de.tastykatana.zeiterfassung/files/test.pdf
+     */
     private void exportStundenzettel() {
         PdfDocument doc = new PdfDocument();
-        doc.close();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(595, 842, 1).create();
 
+        // start a page
+        PdfDocument.Page page = doc.startPage(pageInfo);
+
+        // create main layout for document (this is drawn to the page in the end
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+
+        // create headline for Document and add to main layout
+        TextView headline = new TextView(this);
+        headline.setText(getString(R.string.stundenzettel_headline));
+        headline.setTextSize(TypedValue.COMPLEX_UNIT_PX, 24);
+        mainLayout.addView(headline);
+        headline.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        headline.setGravity(View.TEXT_ALIGNMENT_CENTER);
+
+        // TODO add worktimes to mainLayout
+
+        // do some magic and draw the main layout to the page
+        int measuredWidth = View.MeasureSpec.makeMeasureSpec(575, View.MeasureSpec.EXACTLY);
+        int measuredHeight = View.MeasureSpec.makeMeasureSpec(822, View.MeasureSpec.EXACTLY);
+        mainLayout.measure(measuredWidth, measuredHeight);
+        mainLayout.layout(0, 0, mainLayout.getMeasuredWidth(), mainLayout.getMeasuredHeight());
+        page.getCanvas().translate(20, 20);
+        mainLayout.draw(page.getCanvas());
+
+        // finish the page
+        doc.finishPage(page);
+
+        // write the document content to storage
+        File outFile = new File(this.getFilesDir(), "test.pdf");
+        outFile.getParentFile().mkdirs();
         try {
-            File cacheDir = this.getCacheDir();
-            File file = File.createTempFile("Stundenzettel", "pdf", cacheDir);
+            doc.writeTo(new FileOutputStream(outFile));
         } catch (IOException e) {
             e.printStackTrace();
             Toast.makeText(this, R.string.stundenzettelExportFailed, Toast.LENGTH_SHORT).show();
         }
+
+        doc.close();
+
+        SharingHelper.sharePdf(outFile, this);
     }
 
     @Override
---
+++ app/src/main/java/de/tastykatana/zeiterfassung/SharingHelper.java
@@ -0,0 +1,29 @@
+package de.tastykatana.zeiterfassung;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+import java.io.File;
+
+/**
+ * Created by matthias on 2/12/17.
+ */
+
+public class SharingHelper {
+
+    public static void sharePdf(File pdf, Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setType("application/pdf");
+
+        Uri uri = Uri.parse("file://" + pdf.getAbsolutePath());
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        try {
+            context.startActivity(Intent.createChooser(intent, "Share PDF file"));
+        } catch (Exception e) {
+            Toast.makeText(context, "Error: Cannot open or share created PDF report.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
